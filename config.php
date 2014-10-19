8c1a2f6052ff90729dbb1f689dfb837e0fe9f97c
Pekka Mansikka <peku@pm-netti.com>
2014-10-19 13:28:43 +0300

    Create config.php

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+// DIR
+define('DIR_APPLICATION', '/ your path here /pkeditor/application/');
+define('DIR_SYSTEM', '/ your path here /pkeditor/system/');
+define('DIR_LANGUAGE', '/ your path here /pkeditor/language/');
+define('DIR_TEMPLATE', '/ your path here /pkeditor/template/');
+define('DIR_IMAGE', '/ your path here /pkeditor/image/');
+define("DIR_HOME",$_SERVER['DOCUMENT_ROOT'].'/');
+
+// DB
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'username');
+define('DB_PASSWORD', 'password');
+define('DB_DATABASE', 'database');
+?>
