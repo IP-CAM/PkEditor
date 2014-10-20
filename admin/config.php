9c2e9fd3f0f26f2edf1aa0ed2c0b346e2c0480ee
Pekka Mansikka <peku@pm-netti.com>
2014-10-20 12:24:29 +0300

    Create config.php

---
+++ admin/config.php
@@ -0,0 +1,21 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http:// your domain /pkeditor/');
+define('HTTP_ADMIN', 'http:// your domain /pkeditor/admin/');
+
+
+// DIR
+define('DIR_APPLICATION', '/ your path here /pkeditor/admin/application/');
+define('DIR_SYSTEM', '/ your path here /pkeditor/system/');
+define('DIR_LANGUAGE', '/ your path here /pkeditor/admin/language/');
+define('DIR_TEMPLATE', '/ your path here /pkeditor/admin/template/');
+define('DIR_IMAGE', '/ your path here /pkeditor/admin/image/');
+define('DIR_HOME',$_SERVER['DOCUMENT_ROOT']);
+
+// DB
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'username');
+define('DB_PASSWORD', 'password');
+define('DB_DATABASE', 'database')
+define('DB_PREFIX', 'pk_');
+?>
