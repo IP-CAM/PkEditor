d534a2aa73cc4414fc3477fc85f8fb9eebc435c4
Pekka Mansikka <peku@pm-netti.com>
2014-10-20 12:35:15 +0300

    Create FI.php

---
+++ language/finnish/FI.php
@@ -0,0 +1,86 @@
+<?php
+// Entry
+$_['entry_directory'] = 'Hakemisto:';
+$_['entry_file']      = 'Tiedosto:';
+$_['entry_rename']    = 'Nimeä uudelleen:';
+$_['entry_remove']    = 'Poista:';
+$_['entry_newname']   = 'Muuta nimeä:';
+$_['entry_newfile']   = 'Uuden tiedoston nimi:';
+$_['entry_size']      = 'Koko:';
+
+// Button
+$_['button_cancel']     = 'Peruuta';
+$_['button_create']     = 'Luo tiedosto';
+$_['button_mkdir']      = 'Luo hakemisto';
+$_['button_save']     = 'Tallenna';
+$_['button_up']         = 'Siirry ylempään hakemistoon';
+
+// Text
+$_['text_copyright']      = '&copy;pm-netti.com';
+$_['text_create_file'] = 'Luo uusi tiedosto hakemistoon  <em>%s</em>';
+$_['text_directory']  = 'Hakemisto';
+$_['text_edit']       = 'Muokkaa';
+$_['text_homepage']   = 'Etusivulle';
+$_['text_mkdir']      = 'Luo hakemisto';
+$_['text_no_results'] = 'Tämä hakemisto on tyhjä.';
+$_['text_remove']     = 'Poista';
+$_['text_rename']     = 'Nimeä uudelleen';
+$_['text_rename_dir']     = 'Nimeä hakemisto uudelleen';
+$_['text_title_remove']     = 'Vahvista tiedoston poisto';
+$_['text_title_remove_dir']     = 'Vahvista hakemiston poisto';
+$_['text_utf8_encode'] = 'Näytä teksti Utf-8 merkistökoodauksella.';
+$_['text_warning']    = 'Varoitus';
+
+// Error
+$_['error']['button_close']     = 'Sulje';
+$_['error']['error_create_error']       = 'Tiedoston luominen ei onnistunut';
+$_['error']['error_critical_error']     = 'Kriittinen virhe!';
+$_['error']['error_directory_not_found']          = 'Hakemistoa <em>%s</em> ei löydy!';
+$_['error']['error_file_not_found']          = 'Tiedostoa <em>%s</em> ei löydy!';
+$_['error']['error_invalid_file_permission'] = 'Sinulla ei ole oikeuksia <em>%s</em> tiedostoon.';
+$_['error']['error_invalid_permission'] = 'Sinulla ei ole oikeuksia <em>%s</em> hakemistoon.';
+
+// Shape
+$_['shape_indent']     = 'Lisää sisennystä';
+$_['shape_italic']     = 'Kursivointi';
+$_['shape_strong']     = 'Lihavointi';
+$_['shape_underline']  = 'Alleviivaus';
+
+// Tag
+$_['tag_body']       = 'Body';
+$_['tag_caption']    = 'Caption';
+$_['tag_checkbox']   = 'Checkbox';
+$_['tag_div']        = 'Div';
+$_['tag_form']       = 'Form';
+$_['tag_head']       = 'Head';
+$_['tag_html']       = 'Html  tag';
+$_['tag_input']      = 'Input';
+$_['tag_meta']       = 'Meta';
+$_['tag_select']     = 'Select';
+$_['tag_table']      = 'Table';
+$_['tag_td']          = 'Taulukon solu';
+$_['tag_textarea']   = 'Textarea';
+$_['tag_thead']      = 'Thead';
+$_['tag_title']      = 'Title';
+$_['tag_tr']         = 'Taulukon rivi';
+
+// Php
+$_['php_array']         = 'Array';
+$_['php_arraykeys']     = 'Array keys';
+$_['php_class']       = 'Class';
+$_['php_else']       = 'Else';
+$_['php_elseif']     = 'Elseif';
+$_['php_explode']       = 'Explode';
+$_['php_file_exists']   = 'File exits';
+$_['php_file_get_contents']  = 'File get contents';
+$_['php_for']        = 'For';
+$_['php_foreach']    = 'Foreach';
+$_['php_function']      = 'Funktio';
+$_['php_if']         = 'If';
+$_['php_implode']       = 'Implode';
+$_['php_include']    = 'Include';
+$_['php_opendir']            = 'Opendir & Readdir';
+$_['php_public_method'] = 'Julkinen metodi';
+$_['php_require']    = 'Require';
+$_['php_tag']        = 'PHP tag';
+?>
