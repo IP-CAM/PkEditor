5d53972c3766518cf5a6a427b4ec5a4ae14410a4
Pekka Mansikka <peku@pm-netti.com>
2014-10-20 12:34:40 +0300

    Create GB.php

---
+++ language/english/GB.php
@@ -0,0 +1,86 @@
+<?php
+// Entry
+$_['entry_file']      = 'File'; 
+$_['entry_directory'] = 'Directory';
+$_['entry_remove']    = 'Remove:';
+$_['entry_rename']    = 'Rename';
+$_['entry_size']      = 'Size';
+$_['entry_newname']   = 'Change name of:';
+$_['entry_newfile']   = 'Filename:'; 
+
+// Button
+$_['button_cancel'] = 'Cancel';
+$_['button_create'] = 'Create file';
+$_['button_mkdir'] = 'Create directory';
+$_['button_save']     = 'Save';
+$_['button_up'] = 'Up One Level';
+
+// Text
+$_['text_copyright']      = '&copy;pm-netti.com';
+$_['text_create_file'] = 'Create a new file in the directory <em>%s</em>';
+$_['text_directory'] = 'Directory';
+$_['text_edit'] = 'Edit';
+$_['text_homepage'] = 'Home';
+$_['text_mkdir'] = 'Create directory';
+$_['text_no_results']   = 'This directory is empty.';
+$_['text_remove']     = 'Poista';
+$_['text_rename'] = 'Rename';
+$_['text_rename_dir']     = 'Rename directory';
+$_['text_title_remove']     = 'Confirm the deletion';
+$_['text_title_remove_dir']     = 'Confirm the directory deletion';
+$_['text_utf8_encode'] = 'Show text in UTF-8 character set encoding.';
+$_['text_warning'] = 'Warning';
+
+// Error
+$_['error']['button_close']     = 'Close';
+$_['error']['error_create_error'] = 'File Creation Failed';
+$_['error']['error_critical_error']  = 'Critical error!';
+$_['error']['error_directory_not_found']          = 'Directory <em>%s</em> is not found!';
+$_['error']['error_file not_found'] = 'File <em>%s</em> is not found!';
+$_['error']['error_invalid_file_permission'] = 'You do not have permission to <em>%s</em> file.';
+$_['error']['error_invalid_permission'] = 'You do not have permission to <em>%s</em> directory.';
+
+// Shape
+$_['shape_indent']   = 'Indent';
+$_['shape_strong']     = 'Bold';
+$_['shape_italic']      = 'Italic';
+$_['shape_underline']  = 'Underline';
+
+// Tag
+$_['tag_body']        = 'Body';
+$_['tag_caption']        = 'Caption';
+$_['tag_checkbox']          = 'Checkbox';
+$_['tag_div']            = 'Div';
+$_['tag_form']           = 'Form';
+$_['tag_head']     = 'Head';
+$_['tag_html']     = 'Html';
+$_['tag_input']     = 'Input';
+$_['tag_meta']     = 'Meta';
+$_['tag_select']              = 'Select';
+$_['tag_table']      = 'Table';
+$_['tag_textarea']        = 'Textarea';
+$_['tag_title']      = 'Title';
+$_['tag_td']   = 'Table data';
+$_['tag_thead']         = 'Thead';
+$_['tag_tr']     = 'Table row';
+
+// Php
+$_['php_array']  =  'Array';
+$_['php_arraykeys']  =  'Array keys';
+$_['php_class']         = 'Class';
+$_['php_else']        = 'Else';
+$_['php_elseif']      = 'Elseif';
+$_['php_explode']  =  'Explode';
+$_['php_file_exists']  =  'File exits';
+$_['php_file_get_contents']  =  'File get contents';
+$_['php_for']      = 'For';
+$_['php_foreach']       = 'Foreach';
+$_['php_function']  =  'Function';
+$_['php_if']     = 'If';
+$_['php_implode']  =  'Implode';
+$_['php_include']      = 'Include';
+$_['php_opendir']  =  'Opendir & Readdir';
+$_['php_public_method'] = 'Public method';
+$_['php_require']      = 'Require';
+$_['php_tag']      = 'Php';
+?>
