b546d2919dfc6b01624fd24239b198253d20f1c6
Pekka Mansikka <peku@pm-netti.com>
2014-10-20 12:33:25 +0300

    Create FI.php

---
+++ admin/language/finnish/FI.php
@@ -0,0 +1,49 @@
+<?php
+// Text
+$_['text_add_language']  = 'Lisää kieli';
+$_['text_copyright']      = '&copy;pm-netti.com';
+$_['text_dashboard'] = 'Hallintapaneeli';
+$_['text_default']  = 'Oletus';
+$_['text_delete']    = 'Poista';
+$_['text_edit']       = 'Muokkaa';
+$_['text_home']   = 'Etusivu';
+$_['text_language']  = 'Asenna kieli';
+$_['text_languages']  = 'Asennetut kielet';
+$_['text_to_use']  = 'Vaihda kieli';
+$_['text_add_use']  = 'Ota tämä sivuston kieleksi';
+$_['text_use']  = 'Käytössä';
+
+// Column
+$_['column_action']     = 'Toiminto';
+$_['column_code']     = 'Koodi';
+$_['column_language']  = 'Kielen nimi';
+$_['column_localisation'] = 'Alue';
+
+// Entry
+$_['entry_code']      = 'Koodi:';
+$_['entry_language_code']      = 'Koodi:<br><span>(Kielen kansainvälinen tunnus.)</span>';
+$_['entry_language_localisation']   = 'Alue:<br><span>(Tämä on sama kuin tiedoston nimi ilman päätettä.)</span>';
+$_['entry_language_name']    = 'Nimi: <br/><span>(Tämä on sama kuin hakemiston nimi.)</span>';
+$_['entry_localisation']   = 'Alue:';
+$_['entry_name']    = 'Nimi:';
+
+// Button
+$_['button_add']         = 'Lisää';
+$_['button_cancel']     = 'Peruuta';
+$_['button_save']        = 'Tallenna';
+
+// Text
+$_['text_no_results'] = 'Tämä hakemisto on tyhjä.';
+$_['text_remove']     = 'Poista';
+$_['text_rename']     = 'Nimeä uudelleen';
+$_['text_rename_dir']     = 'Nimeä hakemisto uudelleen';
+$_['text_title_remove']     = 'Vahvista tiedoston poisto';
+$_['text_title_remove_dir']     = 'Vahvista hakemiston poisto';
+$_['text_utf8_encode'] = 'Näytä teksti Utf-8 merkistökoodauksella.';
+$_['text_warning']    = 'Varoitus';
+
+// Error
+$_['error']['button_close']     = 'Sulje';
+$_['error']['error_warning']       = 'Varoitus: Lomakkeen tiedot ovat puutteelliset!';
+
+?>
