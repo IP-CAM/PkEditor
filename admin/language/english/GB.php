d2b975dbbb68656e746d6f3093871c0fcd06a392
Pekka Mansikka <peku@pm-netti.com>
2014-10-20 12:32:15 +0300

    Create GB.php

---
+++ admin/language/english/GB.php
@@ -0,0 +1,38 @@
+<?php
+$_['text_add_language']  = 'Add Language';
+$_['text_copyright']      = '&copy;pm-netti.com';
+$_['text_dashboard'] = 'Dashboard';
+$_['text_default']  = 'Default';
+$_['text_delete']    = 'Delete';
+$_['text_edit']       = 'Edit';
+$_['text_edit_language']  = 'Edit Language';
+$_['text_home']   = 'Home';
+$_['text_language']  = 'Install the language';
+$_['text_languages']  = 'Installed languages';
+$_['text_to_use']  = 'Change language';
+$_['text_add_use']  = 'Change this the language of the site';
+$_['text_use']  = 'In Use';
+
+// Column
+$_['column_action']     = 'Action';
+$_['column_code']     = 'Code';
+$_['column_language']  = 'Language';
+$_['column_localisation'] = 'Localisation';
+
+// Entry
+$_['entry_code']      = 'Code:';
+$_['entry_language_code']      = 'Code:<br><span>(International code for the language.)</span>';
+$_['entry_language_localisation']   = 'Localisation:<br><span>(This is the same as the file name without an extension.)</span>';
+$_['entry_language_name']    = 'Name: <br/><span>(This is the same as the directory name.)</span>';
+$_['entry_localisation']   = 'Localisation:';
+$_['entry_name']    = 'Name:';
+
+// Button
+$_['button_add']         = 'Add';
+$_['button_cancel']     = 'Cancel';
+$_['button_save']        = 'Save';
+
+// Error
+$_['error']['button_close']     = 'Close';
+$_['error']['error_warning']       = 'Warning: The form the data are incomplete!';
+?>
